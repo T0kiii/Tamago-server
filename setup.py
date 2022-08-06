94f35ee7de88d9bb9a26253faaa07d238c2c9bf5
ALberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2022-08-06 11:12:19 +0200

    Dependencias y tarea de compilación añadidos

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Tamago-server",
+    install_requires=[
+        "Werkzeug >= 2.2.0",
+        "Jinja2 >= 3.0",
+        "itsdangerous >= 2.0",
+        "click >= 8.0",
+        "importlib-metadata >= 3.6.0; python_version < '3.10'",
+    ],
+    extras_require={
+        "async": ["asgiref >= 3.2"],
+        "dotenv": ["python-dotenv"],
+    },
+)
