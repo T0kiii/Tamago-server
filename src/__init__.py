c8d30250191b5b189d2ee131a53a02b10bba389f
ALberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2022-08-07 13:48:43 +0200

    Creación esquema BD e inicialización de BD

--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,42 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
-app = Flask(__name__)
 
-@app.route('/')
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'src.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    return app
 
 
-# Esta función correrá nuestro servidor en el server de Flask
-def arranca_tamago_server():
-
-    # TODO: Comprobar si existen datos previos
-    # TODO: Si no existen crear fichero de datos
-    # TODO: Si existen cargar datos y arrancar mascota
-
-    # TODO: Si existen cargar datos y arrancar mascota
-    # TODO: Escuchar eventos, registrar cambios
-
-    return hola()
-
-def hola():
-    return 'holi holu!'
-
-# Solo queremos cargar los datos al arrancar el server e ir guardando periódicamente
-def cargar_datos():
-    return 'holi holu!'
-def guardar_datos():
-    return 'holi holu!'
-
-
-# FUNCIÓN MAIN
-if __name__ == "__main__":
-   app.run()
+# # FUNCIÓN MAIN
+# if __name__ == "__main__":
+#    app.run()
---
+++ src/schema.sql
@@ -0,0 +1,27 @@
+-- Script para generar esctructura de la base de datos
+
+DROP TABLE IF EXISTS mascota;
+-- DROP TABLE IF EXISTS post;
+
+CREATE TABLE mascota (
+  mas_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  mas_nombre TEXT UNIQUE NOT NULL,
+  -- SQLite does not have a separate Boolean storage class. Instead, Boolean values are stored as integers 0 (false) and 1 (true).
+  mas_estaVivo INTEGER UNIQUE NOT NULL,
+  mas_salud REAL UNIQUE NOT NULL,
+  mas_hambre REAL UNIQUE NOT NULL,
+  mas_felicidad REAL UNIQUE NOT NULL,
+  mas_stamina REAL UNIQUE NOT NULL,
+  mas_higiene REAL UNIQUE NOT NULL,
+  mood REAL NOT NULL
+);
+
+-- CREATE TABLE post (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   author_id INTEGER NOT NULL,
+--   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   title TEXT NOT NULL,
+--   body TEXT NOT NULL,
+--   FOREIGN KEY (author_id) REFERENCES user (id)
+-- );
+
